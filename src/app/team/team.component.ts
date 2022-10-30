972e8f7892f00f72bbc21c320bbbb98ce9433091
MancoFranma <manconfranma@gmai.com>
2022-10-29 23:27:51 -0300

    actualizacion back

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css']
 })
+
 export class TeamComponent implements OnInit {
 
-  constructor() { }
+  // private _jsonURL = './doctores.json';
 
+  // constructor(private http: HttpClient){
+
+  // }
+
+  // public getJson(): Observable<any>{
+  //   return this.http.get(this._jsonURL);
+  // }
+
+  // doctorsitos:doctores[]=[];
+  
   ngOnInit(): void {
+    this
   }
-
+  
 }
+
+// export interface doctores{
+//   "Nombre":string,
+//   "Especialidad":string
+// }
